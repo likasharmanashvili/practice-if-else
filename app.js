f94c180ea3b26b078a4b7c9a994f5dba9e8f452c
lika <l.sharmanashvili@ssu.edu.g>
2024-01-18 18:43:49 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// //Write a program that takes the user's age as input and determines whether they are eligible to vote. Print appropriate messages based on the age.
+
+// //Create a program that prompts the user to enter a string. Check if the entered string is empty or not, and print different messages based on the result.
+
+// //Develop a program that asks the user to enter their age and then calculates the ticket price based on the following conditions:
+
+// //Age below 12: Ticket price is 5
+// //Age below 18: Ticket price is 10
+// //Age below 60: Ticket price is 20
+// //Age over 60: Ticket price is 15
+
+
+// //first exercise
+// let userage =  prompt("enter your age.....")
+ 
+// if(userage >= 18){
+//     console.log("you can vote")
+// } else{
+//     console.log("you cant vote")
+// }
+
+// //second exercise
+// let user = prompt("enter text")
+
+// if(user !==""){
+//     console.log("You're registered")
+// } else {
+//     console.log("enter text")
+// }
+
+
+// //third
+
+// let client = prompt("enter your age")
+// if (client < 12){
+//     console.log("ticket price is 5$")
+// } else if (client < 18)
+// {
+//     console.log("ticket price is 10")
+// } else if (client < 60){
+//     console.log("ticket price is 20")
+// }
+// else {
+//     console.log("ticket price is 15")
+    
+// }
+
